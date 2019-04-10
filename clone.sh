2aef29eee42e8878a11b9616e8ee141e209ac46f
Fabian Juette <Fabian.Juette@tu-clausthal.de>
2019-04-10 14:46:53 +0200

    remove all bin folder in clone script

--- clone.sh
+++ clone.sh
@@ -6,6 +6,7 @@ cp -rf ./../$OLDNAME ./../$NEWNAME
 cd ./../$NEWNAME
 rm -Rf .git
 rm -Rf ./src/$OLDNAME.Common/obj
+rm -Rf ./src/$OLDNAME.Common/bin
 rm -Rf ./src/$OLDNAME.Api/obj
 rm -Rf ./src/$OLDNAME.Api/bin
 rm -Rf ./src/$OLDNAME.Persistence/obj
@@ -15,6 +16,7 @@ rm -Rf ./src/$OLDNAME.Domain/bin
 rm -Rf ./src/$OLDNAME.Application/obj
 rm -Rf ./src/$OLDNAME.Application/bin
 rm -Rf ./src/$OLDNAME.Infrastructure/obj
+rm -Rf ./src/$OLDNAME.Infrastructure/bin
 find . -type d -name '*'$OLDNAME'*' -print0 | xargs -0 -n1 bash -c 'mv "$0" "${0/'$OLDNAME'/'$NEWNAME}'"';
 find . -type f -name '*'$OLDNAME'*' -print0 | xargs -0 -n1 bash -c 'mv "$0" "${0/'$OLDNAME'/'$NEWNAME}'"'
 find . -type f -name '*' -exec sed -i "" 's/'$OLDNAME'/'$NEWNAME'/g' {} +
